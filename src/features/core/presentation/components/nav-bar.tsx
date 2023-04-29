ae7cddf1b7c760c5da971e70f4de0a0759fa58ae
SabianF <58588133+SabianF@users.noreply.github.com>
2023-04-29 01:25:57 -0700

    feat: using Bootstrap

--- src/features/core/presentation/components/nav-bar.tsx
+++ src/features/core/presentation/components/nav-bar.tsx
@@ -2,8 +2,11 @@ import Link from "next/link";
 
 export const NavBar = () => {
   return (
-    <nav>
-      <Link href="/">Home</Link>
+    <nav className="nav">
+      <Link className="nav-link active" aria-current="page" href="/">Home</Link>
+      <Link className="nav-link" href="">Link</Link>
+      <Link className="nav-link" href="">Link</Link>
+      <Link className="nav-link disabled" href="">Disabled</Link>
     </nav>
   );
 }
