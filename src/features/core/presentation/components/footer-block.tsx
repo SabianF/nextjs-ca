3826e6f8a851ddbe70f94aab31b42f554bd94c4f
SabianF <58588133+SabianF@users.noreply.github.com>
2023-05-03 23:09:44 -0700

    feat: formatted footer CSS

--- src/features/core/presentation/components/footer-block.tsx
+++ src/features/core/presentation/components/footer-block.tsx
@@ -1,10 +1,10 @@
 import { Block } from "./block"
 
-
+import styles from "./footer-block.module.css";
 
 export const FooterBlock = () => {
   return (
-    <Block>
+    <Block className={[ styles.default ]}>
       <h1>This is the footer</h1>
     </Block>
   );
